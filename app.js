5988a39aaf51eaca5f5e36562258b529891579cb
Maat <maatthieu@gmail.com>
2020-06-29 14:25:39 +0200

    feat(server): error handling

--- app.js
+++ app.js
@@ -1,20 +1,56 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express')
+var path = require('path')
+var cookieParser = require('cookie-parser')
+var logger = require('morgan')
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var isProduction = process.env.NODE_ENV === 'production'
 
-var app = express();
+var indexRouter = require('./routes/index')
+var usersRouter = require('./routes/users')
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+var app = express()
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(logger('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(express.static(path.join(__dirname, 'public')))
 
-module.exports = app;
+app.use('/', indexRouter)
+app.use('/users', usersRouter)
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    var err = new Error('Not Found')
+    err.status = 404
+    next(err)
+})
+
+// development error handler (with stacktrace)
+if (!isProduction) {
+    app.use(function (err, req, res, next) {
+        console.log(err.stack)
+
+        res.status(err.status || 500)
+
+        res.json({
+            errors: {
+                message: err.message,
+                error: err,
+            },
+        })
+    })
+}
+
+// production error handler (without stacktraces)
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500)
+    res.json({
+        errors: {
+            message: err.message,
+            error: {},
+        },
+    })
+})
+
+module.exports = app
