0b19833d62b269b151cb6059cf14510ce4ae8ac4
Maat <maatthieu@gmail.com>
2020-07-05 23:03:24 +0200

    feat(server): add root route

--- app.js
+++ app.js
@@ -2,11 +2,9 @@ var express = require('express')
 var path = require('path')
 var cookieParser = require('cookie-parser')
 var logger = require('morgan')
+const router = require('./routes/createrRouter.js')()
 
 var isProduction = process.env.NODE_ENV === 'production'
-
-var indexRouter = require('./routes/index')
-var usersRouter = require('./routes/users')
 
 var app = express()
 
@@ -16,41 +14,40 @@ app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/', indexRouter)
-app.use('/users', usersRouter)
+app.use('/api', router)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found')
-    err.status = 404
-    next(err)
+  var err = new Error('Not Found')
+  err.status = 404
+  next(err)
 })
 
 // development error handler (with stacktrace)
 if (!isProduction) {
-    app.use(function (err, req, res, next) {
-        console.log(err.stack)
+  app.use(function (err, req, res, next) {
+    console.log(err.stack)
 
-        res.status(err.status || 500)
+    res.status(err.status || 500)
 
-        res.json({
-            errors: {
-                message: err.message,
-                error: err,
-            },
-        })
+    res.json({
+      errors: {
+        message: err.message,
+        error: err,
+      },
     })
+  })
 }
 
 // production error handler (without stacktraces)
 app.use(function (err, req, res, next) {
-    res.status(err.status || 500)
-    res.json({
-        errors: {
-            message: err.message,
-            error: {},
-        },
-    })
+  res.status(err.status || 500)
+  res.json({
+    errors: {
+      message: err.message,
+      error: {},
+    },
+  })
 })
 
 module.exports = app
